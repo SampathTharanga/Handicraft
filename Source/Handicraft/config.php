1743d9751c6ea3eec589bfbd82a57e09c72a8ea4
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-03-12 17:22:01 +0530

    project uploading
    The propose of this project titled "HANDICRAFT E-SHOPER", is a web application project. And developed fr use the OpenCart.

---
+++ Source/Handicraft/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/handicraft/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/handicraft/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/Handicraft/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/Handicraft/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/Handicraft/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/Handicraft/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/Handicraft/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/Handicraft/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/Handicraft/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/Handicraft/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/Handicraft/system/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/Handicraft/system/modification/');
+define('DIR_LOGS', 'C:/xampp/htdocs/Handicraft/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'handicraft');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
